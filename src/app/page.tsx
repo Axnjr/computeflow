1f5d0fbffa81d7a6fd86b1405f381cc4631ab275
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-06-17 10:17:28 +0530

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
-	return (
-		<main>
+// import { use, useState } from "react";
 
-		</main>
-	);
-}
+export default function Home() {
+
+	async function intiateDeployement(formData: FormData) {
+		'use server'
+		const url = formData.get('url')
+		return new Response(JSON.stringify({
+			data: `Deployement initiated for ${url}`
+		}), {
+			status: 200
+		})
+	}
+
+		return (
+			<>
+				<main className="h-screen w-full flex flex-col items-center justify-center gap-5">
+					<h1 className="text-4xl font-black tracking-tight">Cheap Cloud Docker Deployement Service</h1>
+					<form>
+						<input className="h-12 text-left rounded-xl inline-block bg-zinc-100 px-6 outline-none" type="text" placeholder="Docker Container URL .." />
+					</form>
+				</main>
+			</>
+		);
+	}
