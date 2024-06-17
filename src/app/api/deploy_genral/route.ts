431f7f419869aa723b89ee08a7099f87e7bfef8a
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-06-17 18:47:18 +0530

    feat: api endpoint to deploy images to ec2

--- src/app/api/deploy_genral/route.ts
+++ src/app/api/deploy_genral/route.ts
@@ -3,39 +3,22 @@ import { EC2Client, RunInstancesCommand } from "@aws-sdk/client-ec2";
 
 export async function GET(req: NextRequest) {
 
-    const password = req?.nextUrl?.searchParams?.get("password")?.replace(/["\\/]/g, '');
-    const key = req?.nextUrl?.searchParams?.get("key")?.replace(/["\\/]/g, '');
+    // const password = req?.nextUrl?.searchParams?.get("password")?.replace(/["\\/]/g, '');
+    // const key = req?.nextUrl?.searchParams?.get("key")?.replace(/["\\/]/g, '');
 
-    if(password != process.env.PASSWORD) {
-        return new NextResponse("Unauthorized", { status: 401 });
-    }
+    // if(password != process.env.PASSWORD) {
+        // return new NextResponse("Unauthorized", { status: 401 });
+    // }
 
-const userDataScript = `Content-Type: multipart/mixed; boundary="//"
-MIME-Version: 1.0
-
---//
-Content-Type: text/cloud-config; charset="us-ascii"
-MIME-Version: 1.0
-Content-Transfer-Encoding: 7bit
-Content-Disposition: attachment; filename="cloud-config.txt"
-
-#cloud-config
-cloud_final_modules:
-- [scripts-user, always]
-
---//
-Content-Type: text/x-shellscript; charset="us-ascii"
-MIME-Version: 1.0
-Content-Transfer-Encoding: 7bit
-Content-Disposition: attachment; filename="userdata.txt"
-
-#!/bin/bash
+const userDataScript = `#!/bin/bash
 sudo yum update -y
-sudo yum install docker
-sudo service docker start 
-sudo docker pull axnjr/ignition_wssd:1
-sudo docker run -e VALIDATION_TOKEN=${key as string} -p 3000:3000 axnjr/ignition_wssd:1
---//--`;
+sudo yum install -y docker
+sudo service docker start
+sudo docker pull yeasy/simple-web
+sudo docker run -p 80:80 yeasy/simple-web:latest
+`;
+
+// --//--`;
 
     const credentials = {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
@@ -49,14 +32,14 @@ sudo docker run -e VALIDATION_TOKEN=${key as string} -p 3000:3000 axnjr/ignition
 
     const command = new RunInstancesCommand({
         ImageId:"ami-0e1d06225679bc1c5",//"ami-06041499d7ab7c387", 
-        InstanceType:"t4g.micro",
+        InstanceType:"t2.micro",
         MinCount:1,
         MaxCount:1,
         KeyName:"WSSSharedClusterECS",
-        SecurityGroupIds:["sg-0ff0a8d2ad83577c5"],
-        SubnetId:"subnet-07d9a1a6ce4f3d551",
+        SecurityGroupIds:["sg-057a4f9af73086457"],
+        SubnetId:"subnet-0717a6882188d6909",
         Monitoring:{
-            Enabled:true
+            Enabled:true,
         },
         UserData:Buffer.from(userDataScript).toString('base64'),
     });
@@ -65,7 +48,6 @@ sudo docker run -e VALIDATION_TOKEN=${key as string} -p 3000:3000 axnjr/ignition
         const data = await ec2.send(command);
         // console.log(data)
         if(data.Instances) {
-
             return new NextResponse(JSON.stringify({
                 success: true,
                 data: data.Instances[0]
@@ -78,6 +60,27 @@ sudo docker run -e VALIDATION_TOKEN=${key as string} -p 3000:3000 axnjr/ignition
     
 }
 
+// Content-Type: multipart/mixed; boundary="//"
+// MIME-Version: 1.0
+
+// --//
+// Content-Type: text/cloud-config; charset="us-ascii"
+// MIME-Version: 1.0
+// Content-Transfer-Encoding: 7bit
+// Content-Disposition: attachment; filename="cloud-config.txt"
+
+// #cloud-config
+// cloud_final_modules:
+// - [scripts-user, always]
+
+// --//
+// Content-Type: text/x-shellscript; charset="us-ascii"
+// MIME-Version: 1.0
+// Content-Transfer-Encoding: 7bit
+// Content-Disposition: attachment; filename="userdata.txt"
+
+// sudo docker pull axnjr/ignition_wssd:1
+// sudo docker run -e VALIDATION_TOKEN=${key as string} -p 3000:3000 axnjr/ignition_wssd:1
 
 /**
  * sudo rm -rf /var/lib/cloud/*
