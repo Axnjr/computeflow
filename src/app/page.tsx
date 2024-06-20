34a46628d9243a05cdafd3956cf958dedd818a8f
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-06-20 10:43:57 +0530

    progress: app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-// import { use, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
-
-
-		return (
-			<>
-				<main className="h-screen w-full flex flex-col items-center justify-center gap-5">
-					<h1 className="text-4xl font-black tracking-tight">Cheap Cloud Docker Deployement Service</h1>
-					<form>
-						<input className="h-12 text-left rounded-xl inline-block bg-zinc-100 px-6 outline-none" type="text" placeholder="Docker Container URL .." />
-					</form>
-				</main>
-			</>
-		);
-	}
+	return (
+		<>
+			<main className="h-full w-full p-4">
+				{/* <h1 className="text-4xl tracking-tighter m-4">Overview</h1> */}
+			</main>
+		</>
+	);
+}
