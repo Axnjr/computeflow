8fe247331b9974dfccf81af7ea89b52586296fc8
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-06-21 20:27:58 +0530

    feat: client cookies

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,8 +1,13 @@
-"use client"
+// "use client"
 import { SessionProvider } from "next-auth/react"
+import { CookiesProvider } from 'next-client-cookies/server';
 
 export default function AllProvider({ children }: { children: React.ReactNode }) {
 	return (
-		<SessionProvider>{children}</SessionProvider>
+		<SessionProvider>
+			<CookiesProvider>
+				{children}
+			</CookiesProvider>
+		</SessionProvider>
 	)
 }
