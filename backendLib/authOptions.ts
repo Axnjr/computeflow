6a5ee16b48eef21630812b198b08113ed61a49d6
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-06-22 21:30:04 +0530

    refractor: auth config

--- backendLib/authOptions.ts
+++ backendLib/authOptions.ts
@@ -1,11 +1,13 @@
-import NextAuth from "next-auth"
+import NextAuth, { type DefaultSession } from "next-auth"
 import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
 import { XataAdapter } from "@auth/xata-adapter"
 import { XataClient } from "@/xata"
 
+const xata = new XataClient()
+
 export const authConfig = {
-	adapter: XataAdapter(new XataClient()),
+	adapter: XataAdapter(xata),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -16,6 +18,27 @@ export const authConfig = {
 			clientSecret: process.env.GITHUB_CLIENT_SECRET!,
 		})
 	],
+	callbacks: {
+		// @ts-ignore
+		async session({ session, user }) {
+			const account = await xata.db.nextauth_accounts.filter({ 
+				"user.id": user.id, 
+				"provider": "github" 
+			}).getFirst();
+			return {...session, user: {
+				"accessToken": account?.access_token,
+				...user
+			}}
+		}
+	}
 }
+
+declare module "next-auth" {
+	interface Session {
+	  user: {
+		accessToken: string
+	  } & DefaultSession["user"]
+	}
+  }
 
 export const { handlers, signIn, signOut, auth } = NextAuth(authConfig)
