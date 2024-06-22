ef32756fbc2d362659dc35b4b1ab900dcb7504b3
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-06-22 21:30:54 +0530

    feat: apollo provider

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,12 +1,16 @@
 // "use client"
 import { SessionProvider } from "next-auth/react"
 import { CookiesProvider } from 'next-client-cookies/server';
+import { ApolloProvider } from '@apollo/client';
+import client from "@/lib/apolloClient";
 
 export default function AllProvider({ children }: { children: React.ReactNode }) {
 	return (
 		<SessionProvider>
 			<CookiesProvider>
+			<ApolloProvider client={client}>
 				{children}
+			</ApolloProvider>
 			</CookiesProvider>
 		</SessionProvider>
 	)
