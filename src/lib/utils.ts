4611338d0ed3138137c81a028618f80c75e0326e
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-06-23 13:58:53 +0530

    remove: apollo client

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,15 +2,21 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs))
 }
 
 export function monthsPassed(dateString: string) {
-  const givenDate = new Date(dateString);
-  const currentDate = new Date();
-  const timeDifference = currentDate.getTime() - givenDate.getTime();
-  const yearsDifference = currentDate.getFullYear() - givenDate.getFullYear();
-  const monthsDifference = currentDate.getMonth() - givenDate.getMonth();
-  const totalMonthsPassed = yearsDifference * 12 + monthsDifference;
-  return totalMonthsPassed == 0 ? "in this month" : `${totalMonthsPassed} month ago`;
+	const givenDate = new Date(dateString);
+	const currentDate = new Date();
+	const yearsDifference = currentDate.getFullYear() - givenDate.getFullYear();
+	const monthsDifference = currentDate.getMonth() - givenDate.getMonth();
+	let totalMonthsPassed = yearsDifference * 12 + monthsDifference;
+	// if(totalMonthsPassed > 12){
+	// 	let y = 0;
+	// 	while(totalMonthsPassed > 12){
+	// 		totalMonthsPassed = totalMonthsPassed - 12;
+	// 		y += 1;
+	// 	}
+	// }
+	return totalMonthsPassed == 0 ? "in this month" : `${totalMonthsPassed} month ago`;
 }
