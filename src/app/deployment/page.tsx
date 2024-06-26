3d6d013c6dbfacc294d15ac356d71e71c1a0515a
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-06-26 13:47:11 +0530

    feat: deployment page

---
+++ src/app/deployment/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function DeploymentPage({
+    searchParams,
+}: {
+    searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+
+    console.log(searchParams)
+
+    return (
+        <div>page</div>
+    )
+}
