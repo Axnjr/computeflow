79bb76ae8b4fa6683aa0ba901ddf0d34078adad0
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-06-26 13:46:20 +0530

    feat: project config types

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,3 +14,46 @@ type xata = {
     updatedAt: string,
     version: number
 }
+
+export type UserRepo = {
+    name: string,
+    url: string,
+    pushed_at:string,
+    lang: string,
+}
+
+export interface ProjectConfigType extends UserRepo {
+    region?: string,
+    commands: Commands,
+    compute: Compute,
+    env:string
+}
+
+type Commands = {
+    rootDir: string,
+    buildCommands: string[],
+    startCommand: string
+}
+
+type Compute = {
+    instanceType: string,
+    instanceId?: string
+}
+
+export const dummyProjectConfig: ProjectConfigType = {
+    name: '',
+    url: '',
+    pushed_at: '',
+    lang: '',
+    region: '',
+    commands: {
+        rootDir: '',
+        buildCommands: [],
+        startCommand: ''
+    },
+    compute:{
+        instanceType:'',
+        // instanceId:''
+    },
+    env:""
+}
