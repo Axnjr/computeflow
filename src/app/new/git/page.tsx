79eb53ec4ebcfcbef3b27af79d42a8540323269d
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-07-01 12:25:27 +0530

    feat: deployment loading state

--- src/app/new/git/page.tsx
+++ src/app/new/git/page.tsx
@@ -9,7 +9,7 @@ import ProjectConfig from '@/components/projectConfig';
 import SelectCompute from '@/components/selectCompute';
 import EnvVariable from '@/components/envVariable';
 import { ProjectConfigType, dummyProjectConfig } from '@/types/types';
-import { deployInstance } from '@/lib/utils';
+import { addProjectConfigToDatabase, deployInstance } from '@/lib/utils';
 import { useSession } from 'next-auth/react';
 
 export default function DeployViaGithub() {
@@ -21,6 +21,7 @@ export default function DeployViaGithub() {
     pro.current.userId = session.data?.user.id as string
 
     const [projectName, setProjectName] = useState(pro.current.name)
+    const [deploymentStatus, setDeploymentStatus] = useState(false)
     const router = useRouter()
 
     function showSearchedProject(e: React.ChangeEvent<HTMLInputElement>) {
@@ -94,12 +95,40 @@ export default function DeployViaGithub() {
                 pro.current.commands.startCommand = document.getElementById("startcommandinputfeild").value
                 // @ts-ignore
                 pro.current.env = document.getElementById("envvariblesinputfeild").value
-                console.log(pro.current)
+                setDeploymentStatus(true)
                 deployInstance(pro.current)
-                .then(res => router.push(`/${res}?create=true`))
+                .then(res => {
+                    if(res != "Unexpected error occured !"){
+                        pro.current.compute.instanceId = res
+                        addProjectConfigToDatabase(pro.current)
+                        .then(project => {
+                            if(project != "Unexpected error occured !"){
+                                router.push(`/${project}`)
+                            }
+                            else{
+                                alert(project)
+                            }
+                        })
+                    }
+                    else {
+                        alert(res)
+                    }
+                })
             }}
             className='m-auto w-[36%] flex items-center justify-center'>
-                Deploy your app
+                {
+                    deploymentStatus 
+                        ?
+                    <>
+                        <svg className="animate-spin size-[0.9rem] mx-2 text-white dark:text-black inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="3"></circle>
+                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        <span className='text-neutral-500 font-semibold'>Deploying</span>
+                    </>
+                        :
+                    <span className='font-semibold'>Deploy Project</span>
+                }
             </Button>
         </main>
     )
