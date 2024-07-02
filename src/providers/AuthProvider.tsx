cd5a2d90308e6ab4f0716f2524acfc87752c0914
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-07-02 22:06:02 +0530

    refractor: authentication provider

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,12 +1,8 @@
-// "use client"
 import { SessionProvider } from "next-auth/react"
-// import { CookiesProvider } from 'next-client-cookies/server';
-
-
 export default function AllProvider({ children }: { children: React.ReactNode }) {
 	return (
 		<SessionProvider>
-				{children}
+			{children}
 		</SessionProvider>
 	)
 }
