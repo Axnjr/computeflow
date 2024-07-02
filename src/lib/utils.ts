6923fdc9863e11b91c7b09b4aa0e696d4821edae
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-07-02 22:08:07 +0530

    progress:

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,8 @@ import { ProjectConfigType } from "@/types/types";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
@@ -22,13 +24,13 @@ export function monthsPassed(dateString: string) {
 	return totalMonthsPassed == 0 ? "in this month" : `${totalMonthsPassed} month ago`;
 }
 
-export async function deployInstance(projectConfig: ProjectConfigType){
+export async function deployInstance(projectConfig: ProjectConfigType) {
 	let res = await fetch("/api/deploy", {
-		method:"POST",
+		method: "POST",
 		body: JSON.stringify(projectConfig),
 	})
 	if (!res.ok) {
-		console.log("Error occured in deploying project: ",await res.json())
+		console.log("Error occured in deploying project: ", await res.json())
 		return `Unexpected error occured !`
 	}
 	res = await res.json()
@@ -36,9 +38,9 @@ export async function deployInstance(projectConfig: ProjectConfigType){
 	return res?.instanceId
 }
 
-export async function addProjectConfigToDatabase(projectConfig: ProjectConfigType){
+export async function addProjectConfigToDatabase(projectConfig: ProjectConfigType) {
 	let res = await fetch("/api/createUserProject", {
-		method:"POST",
+		method: "POST",
 		body: JSON.stringify(projectConfig),
 	})
 	if (!res.ok) {
@@ -49,4 +51,46 @@ export async function addProjectConfigToDatabase(projectConfig: ProjectConfigTyp
 	// @ts-ignore
 	return res?.projectId
 }
+
+export async function runCommandOnInstance(instanceId: string, commands: string[]) {
+	let res = await fetch("/api/commands", {
+		method: "POST",
+		body: JSON.stringify({
+			instanceId: instanceId,
+			commands: commands
+		}),
+	})
+	if (!res.ok) {
+		console.log("Error occured in creating user project: ", await res.json())
+		return `Unexpected error occured !`
+	}
+	res = await res.json()
+	// @ts-ignore
+	return res?.commandId
+}
+
+export async function getCommandStatus(commandId: string, instanceId: string) {
+	// @ts-ignor
+	// return res?.commandStatus
+	let s;
+	while (true) {
+		let res = await fetch(`/api/commands?commandId=${commandId}&instanceId=${instanceId}`, {cache:"no-cache"})
+		if (!res.ok) {
+			console.log("Error occured in creating user project: ", await res.json())
+			return `Unexpected error occured !`
+		}
+		res = await res.json()
+		// @ts-ignore
+		if (['Success', 'Failed', 'Cancelled'].includes(res?.status)) {
+			// console.log(res?.status)
+			s = res.status;
+			break;
+		}
+
+		console.log(`Command status: ${res?.status}. Waiting for completion...`);
+		await sleep(15000); // Wait for 5 seconds before checking again
+	}
+	return s;
+}
+
 
