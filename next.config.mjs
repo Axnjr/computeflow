7c57c5f1b1c2e2f5dfc8bd830e80fba4c2b9e1ba
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-07-04 22:02:16 +0530

    feat: endpoint rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+export async function rewrites() {
+    return [
+        {
+            source: '/api/execute',
+            destination: 'https://1ad2rray9g.execute-api.ap-south-1.amazonaws.com/stage1/execute', // Proxy to Backend
+        },
+    ];
+}
+
+
+export async function headers() {
+    return [
+        {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" },
+                { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+}
+
+
