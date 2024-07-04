a63ed8234dad60be3cf717b5d70b459804e65e1c
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-07-04 22:01:55 +0530

    perf: fix

--- lambdas/index.mjs
+++ lambdas/index.mjs
@@ -1,64 +1,106 @@
-// VERSION: 17
-import { Client } from 'ssh2';
-
+// VERSION: 29 with new KeyPair & debuging disabled & node-ssh with error handling
+import { NodeSSH } from "node-ssh"
+const ssh = new NodeSSH();
 export async function handler(event) {
     const body = JSON.parse(event.body)
     const host = body.instanceIp;
-    const username = 'ec2-user';
-    const privateKey = process.env.PRIVATE_KEY; // NOT WORKING PASSED THE REAL KEY AS IT IS IN LAMBDA
-    const commands = body.commands;
+    const privateKey = ``; // NOT WORKING PASSED THE REAL KEY AS IT IS IN LAMBDA
+    const script = body.script;
     try {
-        const output = await runSSHCommand(host, username, privateKey, commands);
+        const { stdout, stderr } = await runSSHScript(host, script, privateKey);
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'Commands executed successfully',
-                output: output
-            })
+                message: 'Script executed successfully',
+                stdout: stdout,
+                stderr: stderr
+            }, 2, null)
         };
     } catch (error) {
         console.error('Error running SSH command:', error);
         return {
             statusCode: 500,
             body: JSON.stringify({
-                message: 'Error executing commands',
+                message: 'Error executing script',
                 error: error.message
             })
         };
     }
 }
-function runSSHCommand(host, username, privateKey, commands) {
-    return new Promise((resolve, reject) => {
-        const conn = new Client();
-        let result = [];
-        conn.on('ready', () => {
-            result.push("Connected with remote VM")
-            commands.forEach(command => {
-                conn.exec(command, (err, stream) => {
-                    if (err) {
-                        reject(err);
-                        return;
-                    }
-                    stream.on('close', () => {
-                        conn.end();
-                        resolve(result);
-                    })
-                    .on('data', (data) => {
-                        console.log('STDOUT: ' + data);
-                        result.push(data)
-                    })
-                    .stderr.on('data', (data) => {
-                        console.log('STDERR: ' + data);
-                        result.push(data)
-                    });
-                });
-            });
-        })
-        .connect({
-            host: host,
-            port: 22,
-            username: username,
+async function runSSHScript(ip, script, privateKey) {
+    // console.log(privateKey)
+    try {
+        await ssh.connect({
+            host: ip,
+            username: 'ec2-user',
             privateKey: privateKey
         });
-    });
+        console.log('Client :: ready');
+        const result = await ssh.execCommand(script, {
+            cwd:"/"
+        });
+        console.log('STDOUT: ' + result.stdout);
+        console.log('STDERR: ' + result.stderr);
+        ssh.dispose();
+        return { stdout: result.stdout, stderr: result.stderr };
+    } catch (err) {
+        console.error('SSH Connection Error:', err);
+        throw err;
+    }
 }
+
+// let t = await handler({
+//     body:{
+//         instanceIp:"13.235.24.46",
+//         instanceId:"rec_3we45r6jk",
+//         script:
+// `sudo yum update -y
+// sudo yum install git -y
+// sudo yum install nodejs -y npm
+// sudo git clone -v https://github.com/Axnjr/starter-express-api radha/ec2
+// cd radha/ec2
+// sudo npm install
+// sudo npm install -g forever
+// sudo forever start index.js`}
+// })
+// console.log(t)
+// return new Promise((resolve, reject) => {
+//     const conn = new Client();
+//     let stdout = '';
+//     let stderr = '';
+//     conn.on('ready', () => {
+//         console.log('Client :: ready');
+//         stdout += "Connected with remote VM ! \n"
+//         conn.exec(script, (err, stream) => {
+//             if (err) {
+//                 reject(err);
+//                 return;
+//             }
+//             stream.on('close', (code, signal) => {
+//                 console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+//                 conn.end();
+//                 resolve({ stdout, stderr });
+//             }).on('data', (data) => {
+//                 console.log('STDOUT: ' + data);
+//                 stdout += data;
+//             }).stderr.on('data', (data) => {
+//                 console.log('STDERR: ' + data);
+//                 stderr += data;
+//             }).on('banner', (message) => {
+//                 console.log('SSH Banner:', message);
+//             })
+//             .on('greeting', (message) => {
+//                 console.log('SSH Greeting:', message);
+//             })
+//             ;
+//         });
+//     }).connect({
+//         host: ip,
+//         port: 22,
+//         username: "ec2-user",
+//         privateKey: privateKey,
+//         // debug: (info) => {
+//         //     console.log("DEBUG:", info)
+//         // }
+//     });
+// });
